f9e4a48caf8b1cb5376d229aaebcd69803519738
Affandi Pratama <60037959+afanditama12@users.noreply.github.com>
2021-12-10 12:55:27 +0700

    tambah mencari, tinggi, ukuran, nilai maksimum,

--- src/program/tree/BinaryTree.java
+++ src/program/tree/BinaryTree.java
@@ -7,6 +7,10 @@ public class BinaryTree {
     public static void main(String[] args) {
         GenericTree tree = new GenericTree();
         tree.display();
+        System.out.println(tree.height());
+        System.out.println(tree.size());
+        System.out.println(tree.max());
+        System.out.println(tree.find(3));
     }
 }
 
@@ -23,6 +27,7 @@ class GenericTree {
 
     }
 
+    // menambah element tree
     private Node root;
 
     GenericTree() {
@@ -51,20 +56,86 @@ class GenericTree {
         return node;
     }
 
-    public void display(){
+    public void display() {
         display(this.root);
     }
 
-    private void display(Node node){
+    private void display(Node node) {
         String str = node.data + " => ";
 
-        for(Node child: node.children){
+        for (Node child : node.children) {
             str += child.data + ", ";
         }
         System.out.println(str);
 
-        for(Node child: node.children){
+        for (Node child : node.children) {
             display(child);
         }
+    }
+
+    // mencari tinggi tree
+    public int height() {
+        return this.height(root);
+    }
+
+    private int height(Node node) {
+        int th = 0;
+
+        for (Node child : node.children) {
+            int ch = height(child);
+            if (ch > th) {
+                th = ch;
+            }
+        }
+        return th + 1;
+    }
+
+    // mencari ukuran tree
+    public int size() {
+        return this.size(this.root);
+    }
+
+    private int size(Node node) {
+        int ts = 1;
+
+        for (Node nn : node.children) {
+            int cs = size(nn);
+            ts += cs;
+        }
+        return ts;
+    }
+
+    // max (large element)
+    public int max() {
+        return this.max(this.root);
+    }
+
+    private int max(Node node) {
+        int tm = node.data;
+
+        for (Node child : node.children) {
+            int cm = max(child);
+            if (cm > tm)
+                tm = cm;
+        }
+        return tm;
+    }
+
+    // mencari elemen di tree
+    public boolean find(int element){
+        return find(this.root, element);
+    }
+    
+    private boolean find(Node node, int element){
+        if(node.data == element){
+            return true;
+        }
+
+        for(Node child: node.children){
+            if(find(child, element)){
+                return true;
+            }
+        }
+        return false;
     }
 }
